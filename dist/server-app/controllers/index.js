5af801d8ad4b5ddc6ffffa443de15a79cf7c0f12
EndyKaufman <admin@site15.ru>
2019-05-07 12:04:03 +0500

    chore: Remove builded files

--- dist/server-app/controllers/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const contact_controller_1 = require("./contact.controller");
-exports.APP_CONTROLLERS = [
-    contact_controller_1.ContactController
-];
-//# sourceMappingURL=index.js.map
